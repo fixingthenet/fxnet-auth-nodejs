999bf78cb3b9bbf23fae33fae69f66aa92421669
Peter Schrammel <peter.schrammel@gmx.de>
2018-11-02 22:58:36 +0000

    seeding admin password

--- src/seeders/20181031232653-admin-user.js
+++ src/seeders/20181031232653-admin-user.js
@@ -1,9 +1,12 @@
-'use strict';
+var utils=require('../lib/utils');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+      var password = utils.stringGen(10);
+      console.log("Admin password:", password);
       return queryInterface.bulkInsert('users', [{
         login: 'admin',
+        password: password,
         created_at: new Date,
         updated_at: new Date,
       }], {});
