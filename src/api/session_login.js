9e1209460cb2d78ac35545acf1ef214eb5ac93e7
Peter Schrammel <peter.schrammel@gmx.de>
2018-12-15 23:33:38 +0000

    jwt  login

--- src/api/session_login.js
+++ src/api/session_login.js
@@ -1,9 +1,27 @@
+import jwt from 'jsonwebtoken'
+import fs from 'fs'
+
+// https://github.com/auth0/node-jsonwebtoken
+const cert= fs.readFileSync('config/secrets/key.pem');
+const exp='7d';
+const issuer='https://dev-auth2.fixingthe.net/';
+
 export default async function(_root, ctx, login, password) {
 
     if (login && password) {
         var user = await ctx.models.User.findOne({where: { login: login}});
         if (user && user.validPassword(password)) {
-            return { token: 'okokok2'}
+            var token = jwt.sign(
+                { user: { id: user.id
+                        }
+                },
+                cert,
+                { algorithm: 'RS256',
+                  expiresIn: exp,
+                  issuer: issuer
+                }
+            )
+            return { token: token }
         } else {
             return {
                 errors: {
