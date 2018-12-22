708c2e20ca84f69d7b3c7135fb2cf4713c7e0e30
Peter Schrammel <peter.schrammel@gmx.de>
2018-12-22 23:17:18 +0000

    first console implementation

--- src/main.js
+++ src/main.js
@@ -1,29 +1,3 @@
-//middleware
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express();
-//const { applyMiddleware } = require('graphql-middleware');
-import server from './server';
-import models from './models';
+import app from './app'
 
-async function start() {
-    // Make sure the database tables are up to date
-    //    await models.sequelize.authenticate();
-    var options = {
-        port: 3000,
-    }
-    // Start the GraphQL server
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(cors());
-    server.applyMiddleware({ app });
-    app.listen(options,() => {
-        console.log(`Server is running on localhost`);
-    })
-    await models.User.setup();
-}
-
-start();
-console.log(server)
+app.start(true);
